0ab4c0f965e3764d55b65e5f6eddc1187ab15c03
flipflurp <ethanleemiao308@gmail.com>
2026-01-19 00:37:48 -0800

    added dash
    added a dash but its not where i would like it, the dash has too much momentum in the air but im too tired to fix atm

--- reap-and-sew/scenes/lumin.gd
+++ reap-and-sew/scenes/lumin.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var jump_speed := 200.0
 @export var walk_speed := 100.0
 @export var run_speed := 200.0
+@export var dash_speed:= 200.0
 
 @export var ground_accel := 10.0
 @export var air_accel := 10.0
@@ -12,7 +13,9 @@ extends CharacterBody2D
 var wish_dir := 0.0
 var wish_speed := 0.0
 var jump_queue := false
-
+var can_dash := true
+var dash_dir := 1
+var dash_cd:=0.0
 # mouse capture shite
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -26,7 +29,7 @@ func input_buffer(pressed, released, queue) -> bool:
 		return false
 	return pressed or queue
 
-func apply_friction(delta: float) -> void:
+func apply_friction(delta: float) -> void: ## Applies Friction
 	var speed: float = abs(velocity.x)
 	if speed == 0.0:
 		return
@@ -41,7 +44,7 @@ func apply_friction(delta: float) -> void:
 	if speed > 0.0:
 		velocity.x *= new_speed / speed
 
-# quake acceleration
+## quake acceleration
 func accelerate(accel: float, delta: float) -> void:
 	var current_speed := velocity.x * wish_dir
 	var add_speed := wish_speed - current_speed
@@ -66,16 +69,31 @@ func _handle_air_physics(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	var input_x := Input.get_axis("left", "right")
+
 	wish_dir = sign(input_x)
+	
+	
+	@warning_ignore("incompatible_ternary") # idk why its raising this, i think theyre both ints?
+	dash_dir = wish_dir if wish_dir != 0 else dash_dir
+	
 	
 	# expected max speed
 	wish_speed = run_speed if Input.is_action_pressed("run") else walk_speed
-
+	
+	
+	if Input.is_action_pressed("dash") and can_dash:
+		can_dash = false
+		dash_cd = 1
+		velocity.x += dash_speed * dash_dir
+	dash_cd -= delta
+	
+	if dash_cd <= 0:
+		can_dash = true
 	jump_queue = input_buffer(Input.is_action_just_pressed("jump"), Input.is_action_just_released("jump"), jump_queue)
-
+	
 	if is_on_floor():
 		if jump_queue:
-			velocity.y = -jump_speed	# boing
+			velocity.y = -jump_speed	# boing boing
 			jump_queue = false
 		_handle_ground_physics(delta)
 	else:
